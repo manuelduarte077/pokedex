fce8ad049308c1ef92818c50b6655d577c96211f
@manuelduarte077 <carlosd99m@gmail.com>
2022-07-18 00:49:07 -0600

    home screen design

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { Image, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { styles } from '../../theme/appTheme';
 
 const HomeScreen = () => {
-  return <Text>Home Screen</Text>;
+  const { top } = useSafeAreaInsets();
+
+  return (
+    <>
+      <Image
+        source={require('../../../assets/pokebola.png')}
+        style={styles.pokebolaBG}
+      />
+
+      <Text
+        style={{
+          ...styles.title,
+          ...styles.globalMargin,
+          top: top + 20,
+        }}>
+        Pokedex
+      </Text>
+    </>
+  );
 };
 
 export default HomeScreen;
