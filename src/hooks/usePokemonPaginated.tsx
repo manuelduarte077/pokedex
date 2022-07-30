6ac025e7e3be75502dd8720f53add5e86b5190f1
manuelduarte077 <carlosd99m@gmail.com>
2022-07-30 15:30:19 -0600

    chore: datatype for request HTTP

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,13 +1,29 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { pokemonApi } from '../api/pokemonApi';
+import {
+  PokemonPaginatedResponse,
+  SimplePokemon,
+  Result,
+} from '../interfaces/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>(
+    [],
+  );
+
   const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40/');
 
   const loadPokemons = async () => {
-    const resp = await pokemonApi.get(nextPageUrl.current);
+    const resp = await pokemonApi.get<PokemonPaginatedResponse>(
+      nextPageUrl.current,
+    );
 
     nextPageUrl.current = resp.data.next;
+    mapPokemonList(resp.data.results);
+  };
+
+  const mapPokemonList = (pokemonList: Result[]) => {
+    pokemonList.forEach(poke => console.log(poke.url));
   };
 
   useEffect(() => {
