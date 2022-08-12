e3d2c9b960547704f5e61430447572b1fbd38630
Manuel Duarte <carlosd99m@gmail.com>
2022-08-12 07:15:38 -0600

    ui: search component (#6)

--- src/screens/search/SearchScreen.tsx
+++ src/screens/search/SearchScreen.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Platform } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { SearchInput } from '../../components/SearchInput';
 
 export const SearchScreen = () => {
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>Search</Text>
+    <View
+      style={{
+        flex: 1,
+        marginTop: Platform.OS === 'ios' ? top : top + 10,
+        marginHorizontal: 20,
+      }}>
+      <SearchInput />
     </View>
   );
 };
